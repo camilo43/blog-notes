0b13ce8f25bfbd4402bbc7e3f1383b8be435918c
Juan Camilo Vega <juancvegao@iMac-de-Juan.local>
2022-10-24 20:54:38 +1100

    Backend focus

--- index_express.js
+++ index_express.js
@@ -82,8 +82,8 @@ app.post('/notes', (request, response) => {
     }
   )
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  const PORT = process.env.PORT || 3001
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 
